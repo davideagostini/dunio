c31e0f82c6f681ede260437a4a8fd479fa338ce0
Davide Agostini <dsagostini@gmail.com>
2026-03-27 01:25:53 +0100

    remove unused imports

--- app/src/main/java/com/davideagostini/summ/ui/entries/EntriesScreen.kt
+++ app/src/main/java/com/davideagostini/summ/ui/entries/EntriesScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
