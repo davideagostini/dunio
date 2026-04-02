9b9a110fd3d5c5aa55f35b52344f7c9809ca78c4
Davide Agostini <dsagostini@gmail.com>
2026-04-02 21:49:02 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ plugins {
     alias(libs.plugins.google.services)
 }
 
-val appVersionName = "0.1.3"
-val appVersionCode = 15
+val appVersionName = "0.1.4"
+val appVersionCode = 16
 
 val keystoreProperties = Properties()
 val keystorePropertiesFile = rootProject.file("keystore.properties")
