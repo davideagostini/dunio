bec377ccfa8100748b934cfdddfc5c50e3482980
Davide Agostini <dsagostini@gmail.com>
2026-04-23 21:06:36 +0200

    Bump app version to 0.1.15 (27)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ plugins {
     alias(libs.plugins.google.services)
 }
 
-val appVersionName = "0.1.14"
-val appVersionCode = 26
+val appVersionName = "0.1.15"
+val appVersionCode = 27
 
 val keystoreProperties = Properties()
 val keystorePropertiesFile = rootProject.file("keystore.properties")
