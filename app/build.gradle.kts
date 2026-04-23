8fc6f15112ed71616b1626b12745576f3a00b6f0
Davide Agostini <dsagostini@gmail.com>
2026-04-23 20:57:36 +0200

    Update app to Navigation 3 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -155,7 +155,6 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.viewmodel.navigation3)
     implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.navigation3.runtime)
     implementation(libs.androidx.navigation3.ui)
 
